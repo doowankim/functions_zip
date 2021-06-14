ab0e72d952e5fb1891d36cca74df0cb55c84187b
doowankim <rlaendhks11@gmail.com>
2021-06-14 16:58:37 +0900

    [ADD] prettier, globalstyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import Router from './router';
+import { createGlobalStyle } from 'styled-components';
+
+const Container = createGlobalStyle`
+	body {
+		margin: 0;
+		padding: 0;
+	}
+`;
 
 const App = () => {
-	return (
-		<div>
-			<Router />
-		</div>
-	);
+  return (
+    <div>
+      <Container />
+      <Router />
+    </div>
+  );
 };
 
 export default App;
